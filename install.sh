09459a24c21406bed3a1c0bd1d6b6a813fc0787a
bhavaniravi <bhava0895@gmail.com>
2022-07-28 08:44:09 +0530

    update

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:
 echo "source ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ${ZDOTDIR:-$HOME}/.zshrc
 rm -fr zsh-syntax-highlighting
 rm -fr zsh-autosuggestions
-
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 brew install fzf
 echo "alias python=/usr/local/bin/python3" >> ~/.zshrc
 echo "alias pip=/usr/local/bin/pip3" >> ~/.zshrc
@@ -23,11 +23,11 @@ brew install --cask obsidian
 git config --global user.name "Bhavani Ravi"
 git config --global user.email bhava0895@gmail.com
 
-ssh-keygen -t rsa -C "bhava0895@gmail.com"
+#ssh-keygen -t rsa -C "bhava0895@gmail.com"
 
-cat ~/.ssh/id_rsa.pub 
+#cat ~/.ssh/id_rsa.pub 
 
-
+brew install --cask visual-studio-code
 cat vscode-extensions.txt | while read extension || [[ -n $extension ]];
 do
   code --install-extension $extension --force
